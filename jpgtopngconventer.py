273b635c826538aa98ac7ac4c7e28ebba7557aab
industArk <achoruzy@gmail.com>
2021-01-29 11:14:58 +0100

    Initial commit

---
+++ jpgtopngconventer.py
@@ -0,0 +1,17 @@
+import sys
+from os import scandir
+from PIL import Image
+
+# Grab sys.argv arguments
+
+# folder_from = sys.argv[1]
+# folder_to = sys.argv[2]
+
+
+# check if /new exists and if not - create it
+
+
+# Function for converting file from jpg to png
+
+
+# Loop over files in /Pokedex folder
